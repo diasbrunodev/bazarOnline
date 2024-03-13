e1b53c754803b809ec33e67862a3e2ab46058a6a
Bruno Dias <brunodias14319@gmail.com>
2024-03-13 11:24:28 -0300

    13/03/24 11:24

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import { DivImage, HeaderBar } from './styles'
 
 import logo from '../../images/logo_tropinha_oficial_site.png'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <>
       <HeaderBar>
         <DivImage>
-          <img src={logo} alt="Logo Bazar online" />
+          <Link to="/cadastro">
+            <img src={logo} alt="Logo Bazar online" />
+          </Link>
         </DivImage>
 
         <div>Bazar Tropinha</div>
